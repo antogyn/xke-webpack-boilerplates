2f9033d96b8c8f6eb95c2a64e50e7e88f3b8c530
antogyn <antogyn@gmail.com>
2016-02-24 19:22:09 +0100

    Add lazy-loading example

--- public/app.js
+++ public/app.js
@@ -1,1 +1,9 @@
 import './components/my-foo/my-foo';
+import $ from 'jquery'
+
+$('#mybutton').click(() => {
+  require.ensure([], () => {
+    const mybutton = require('./mybutton').default;
+    mybutton.makeItYellow();
+  });
+});
---
+++ public/mybutton.js
@@ -0,0 +1,7 @@
+import $ from 'jquery';
+
+export default {
+  makeItYellow() {
+    $('body').css('background-color', 'yellow');
+  }
+};
